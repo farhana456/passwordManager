4f8427e767f8ee92f0284ea38051cc07e039c82f
farhana456 <farhanafiha2004@gmail.com>
2025-02-10 19:47:00 +0600

    changes

--- main.py
+++ main.py
@@ -1,46 +1,134 @@
+import json
 from tkinter import *
+from tkinter import messagebox
+from random import choice, shuffle, randint
+import pyperclip
 
-# উইন্ডো তৈরি
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+               'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    symbols = ['!', '.', '-', '_', '@', '#', '$', '%', '&', '*']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+
+    password_list = password_symbols + password_numbers + password_letters
+    shuffle(password_list)
+
+    password_str = "".join(password_list)
+    password_input.insert(0, password_str)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_input.get()
+    username = username_input.get()
+    password = password_input.get()
+
+    # Validate Entries
+    if validate_entries(website, username, password):
+        new_data = {website: {
+            "email": username,
+            "password": password
+        }}
+        print(new_data)
+        pyperclip.copy(password)
+        try:
+            # Write information to data file
+            with open("data.json", "r") as file:
+                # Read/Import json to python dict
+                data = json.load(file)
+        except FileNotFoundError:
+            # Create new data.json file
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            # If file exists, Update dict to add new data
+            data.update(new_data)
+            with open("data.json", "w") as file:
+                # Add python dict to json
+                json.dump(data, file, indent=4)
+        finally:
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+            display_messagebox("Your info was added successfully!")
+    else:
+        display_messagebox("Please make sure to not leave any fields empty.")
+
+
+def display_messagebox(message_text):
+    messagebox.showinfo(title="Password Generator", message=message_text)
+
+
+def validate_entries(website, username, password):
+    if len(website) < 2 or '@' not in username or len(password) < 2:
+        return False
+    else:
+        return True
+
+
+def find_password():
+    website = website_input.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        display_messagebox("No data file found.")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            display_messagebox(f"Email: {email} \nPassword: {password}")
+            pyperclip.copy(password)
+        else:
+            display_messagebox(f"No details for the {website} exists.")
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.config(padx=20, pady=50)
 
-# ক্যানভাস (Logo Image)
-canvas = Canvas(width=200, height=200)
+logo = Canvas(width=202, height=202)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=0, columnspan=3)
+logo.create_image(100, 100, image=logo_img)
+logo.grid(row=0, column=1)
 
-# Website Label & Entry
+# Website Input
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
-website_entry.focus()  # কার্সর স্বয়ংক্রিয়ভাবে এখানে থাকবে
+website_input = Entry(width=21)
+website_input.grid(row=1, column=1)
+website_input.focus()
 
-# Email/Username Label & Entry
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
+website_search = Button(text="Search", width=13, command=find_password)
+website_search.grid(row=1, column=2)
 
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "your_email@gmail.com")  # ডিফল্ট ইমেইল দেখানোর জন্য
+# Username Input
+username_label = Label(text="Email/Username:")
+username_label.grid(row=2, column=0)
 
-# Password Label & Entry
+username_input = Entry(width=38)
+username_input.grid(row=2, column=1, columnspan=2)
+username_input.insert(0, "email@example.com")
+
+# Password Input
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_input = Entry(width=21)
+password_input.grid(row=3, column=1)
 
-# Generate Password Button
-generate_button = Button(text="Generate Password")
-generate_button.grid(row=3, column=2)
+password_btn = Button(text="Generate Password", width=13, command=generate_password)
+password_btn.grid(row=3, column=2)
 
-# Add Button
-add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1, columnspan=2)
+# Add to list button
+add_info_btn = Button(text="Add", width=36, command=save)
+add_info_btn.grid(row=4, column=1, columnspan=2)
 
-# উইন্ডো চালু রাখা
 window.mainloop()
